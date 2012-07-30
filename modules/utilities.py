ad357cb7846a893d15ab108826f00165c64fe105
Kale Kundert <kale@thekunderts.net>
2012-07-29 18:13:53 -0700

    Create a timer class in the utilities module.

---
+++ modules/utilities.py
@@ -0,0 +1,34 @@
+class Timer:
+
+    def __init__(self, duration, *callbacks):
+        self.duration = duration
+        self.callbacks = list(callbacks)
+
+        self.elapsed = 0
+        self.paused = False
+
+    def register(self, callback):
+        self.callbacks.append(callback)
+
+    def unregister(self, callback):
+        self.callbacks.remove(callback)
+
+    def pause(self):
+        self.paused = True
+
+    def unpause(self):
+        self.pause = False
+
+    def update(self, time):
+
+        if self.elapsed > self.duration:
+            return
+
+        self.elapsed += time
+
+        if self.elapsed > self.duration:
+
+            for callback in self.callbacks:
+                callback()
+
+
